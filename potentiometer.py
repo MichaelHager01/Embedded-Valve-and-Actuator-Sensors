82a69cd704c7d66025c75331134312a227afe9e5
Michael Hager <michaelhswe@gmail.com>
2024-04-08 12:08:06 -0500

    Add files via upload

--- potentiometer.py
+++ potentiometer.py
@@ -2,9 +2,11 @@ import time
 import busio
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
+import csv
 
 
 '''
+POTENTIOMETER
 LEFT 	----> 	RIGHT
 RED 	ORANGE 	BLACK
 GROUND 	GPIO 	5V
@@ -17,7 +19,7 @@ SDA_PIN = 2
 
 # Define the range of degrees of rotation
 MIN_DEGREES = 0
-MAX_DEGREES = 230
+MAX_DEGREES = 270
 
 # Define the maximum ADC value
 MAX_ADC_VALUE = 2**15
@@ -29,16 +31,27 @@ i2c = busio.I2C(SCL_PIN, SDA_PIN)
 ads = ADS.ADS1115(i2c, address=0x48)
 
 # Define the analog input channel
-channel = AnalogIn(ads, ADS.P2)
+channel = AnalogIn(ads, ADS.P3)
 
 # Function to map ADC values to degrees of rotation
 def map_to_degrees(value):
     return (value / MAX_ADC_VALUE) * (MAX_DEGREES - MIN_DEGREES)
 
-# Loop to read the analog input continuously
-while True:
-    analog_value = channel.value
-    degrees = map_to_degrees(analog_value)
-    print("Analog Value:", analog_value, "Degrees of Rotation:", degrees)
-    time.sleep(0.1)
+# Define time variables for csv file
+time_pass = 0
+time_increment = 0.1
+
+# Create csv to write data into
+with open("potentiometer_data_40.csv", 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(["Time(s)", "Degrees of Rotation"])
+    
+    # Loop to read the analog input continuously
+    while True:
+        analog_value = channel.value
+        degrees = map_to_degrees(analog_value)
+        print("Degrees of Rotation:", degrees)
+        csvwriter.writerow([f"{time_pass:.1f}", degrees])
+        time.sleep(time_increment)
+        time_pass += time_increment
 
